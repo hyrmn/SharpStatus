c06f65d56843f868345c94c6b0c2cec6a3f632e4
Ben Hyrman <ben@hyr.mn>
2022-10-27 11:15:58 +0200

    Progress on moving files around

--- app/src/SharpStatusApp/Pages/Index.cshtml.cs
+++ app/src/SharpStatusApp/Pages/Index.cshtml.cs
@@ -1,12 +1,50 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
 namespace SharpStatusApp.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            /// <summary>
+            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
+            ///     directly from your code. This API may change or be removed in future releases.
+            /// </summary>
+            [Display(Name = "Remember me?")]
+            public bool RememberMe { get; set; }
+        }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+
+            return Page();
         }
     }
 }
